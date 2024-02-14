ba8a9af6538c46bdece3d99e8622f006a84d02c6
EbenBoakye <117763566+EbenBoakye@users.noreply.github.com>
2024-02-14 17:55:33 +0000

    Added top Match page

---
+++ lib/screens/top_match.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:shopby/screens/landing_page.dart';
+import 'package:shopby/screens/product_search.dart';
+
+class TopMatch extends StatelessWidget {
+  const TopMatch(Key? key) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+     return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        // You can set the background color to match your Scaffold's background or use a different color
+        backgroundColor: Colors.blue,
+        // Add leading IconButton that opens a drawer
+        leading: IconButton(
+          icon: const Icon(Icons.menu), // The hamburger "menu" icon
+          onPressed: () {
+            Scaffold.of(context).openDrawer();
+            // openDrawer();
+          },
+        ),
+        title: Image.asset('/Users/student/dev/projects/shopby/assets/images/shopby.png', fit: BoxFit.cover, width: 250, height: 250),
+        // If you want to remove the shadow, set elevation to 0
+        elevation: 0,
+      ),
+     );
+  }
+}
