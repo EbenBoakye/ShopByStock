b23f3afc8c6efaa638d085bcc32e225b3ee37e99
EbenBoakye <117763566+EbenBoakye@users.noreply.github.com>
2024-02-19 18:19:27 +0000

    testWidgets

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:shopby/main.dart';
+import 'package:shopby/screens/landing_page.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ShopBystock());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
